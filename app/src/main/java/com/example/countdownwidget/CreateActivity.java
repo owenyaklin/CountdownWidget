751cd11b018a2eebff857d795a74cf7e797bd23e
owenyaklin <owen.yaklin@gmail.com>
2024-04-04 14:24:07 -0500

    Add Toolbar and back button to Create Activity

--- app/src/main/java/com/example/countdownwidget/CreateActivity.java
+++ app/src/main/java/com/example/countdownwidget/CreateActivity.java
@@ -1,8 +1,9 @@
 package com.example.countdownwidget;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.countdownwidget.ui.create.CreateFragment;
 
@@ -13,9 +14,15 @@ public class CreateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, CreateFragment.newInstance())
-                    .commitNow();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, CreateFragment.newInstance()).commitNow();
+        }
+
+        Toolbar toolbar = findViewById(R.id.toolbar_create);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
 }
